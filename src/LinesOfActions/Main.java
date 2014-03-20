8ad681f50bb3d6104635c73fadf8784ccfda8b15
Julien Drolet-Noel <eurojulien@gmail.com>
2014-03-20 15:13:44 -0400

    Main
    Ajout d'un timer en millisecondes pour connaitre notre temps de traitement
    Le thread principal verifie si l'arbre est termine a chaque 100 millisecondes

--- src/LinesOfActions/Main.java
+++ src/LinesOfActions/Main.java
@@ -12,6 +12,8 @@ public class Main extends Thread{
 	private static IA megaMind = null;
 	private static MiniMax miniMax;
 	private static int playerColor = 0;
+	private static long startTime = 0;
+	private static long endTime = 0;
 	
 	private static String theirLastMove = "";
 	private static String ourLastMove	= "";
@@ -73,6 +75,9 @@ public class Main extends Thread{
 			// Reception de la reponse du serveur
 			server.getServerCommand();
 			
+			// Demarrage du timer
+			startTime = System.nanoTime();
+			
 			//megaMind.notifyMovementEnemyTeam(server.getLastTurn().trim());
 			//megaMind.run();
 			
@@ -85,7 +90,7 @@ public class Main extends Thread{
 			// de traitement de IA, il nous est possible de faire d'autre changements aussi.
 			do{
 				try {
-					Thread.sleep(1);
+					Thread.sleep(100);
 				} catch (InterruptedException e) {}
 			} while(!MiniMax.bestMoveHasBeenFound());
 			
@@ -100,14 +105,14 @@ public class Main extends Thread{
 			// Envoie de la reponse
 			//server.sendServerCommand(megaMind.getBestMove());
 			//megaMind.notifyMovementMyTeam(megaMind.getBestMove());
-			
-			System.out.println("Coup envoye : " + miniMax.getBestMove());
+			endTime = System.nanoTime();
 			
 			server.sendServerCommand(miniMax.getBestMove());
 			miniMax.getIA().notifyMovementMyTeam(miniMax.getBestMove());
-			miniMax.getIA().drawBoard(false);
-			
-			// TODO : Traitement supplementaire lorsque l'adversaire joue
+			System.out.println("Coup envoye : " + miniMax.getBestMove());
+			System.out.println("Temps demande : " + + (endTime - startTime)/(1000000) + " milliseconds");
+			System.out.println("Nombre d'elagage : " + MiniMax.nombreElagage);
+			//miniMax.getIA().drawBoard(false);
 			
 		}while(true);
 	}
