3060e17c5ca71c0dd6712baa24f259a95bd2a7e5
Julien Drolet-Noel <eurojulien@gmail.com>
2014-03-30 13:56:14 -0400

    SyncThread
    Nouvelle variable de synchronisation des threads

--- src/MiniMax/SyncThread.java
+++ src/MiniMax/SyncThread.java
@@ -4,6 +4,21 @@ public class SyncThread {
 
 	// Variable partagee par les threads
 	public static boolean bestMoveHasBeenFound 			= false;
+	public static boolean computationTimeIsFinished		= false;
 	public static boolean victoryOrDefautHasBeenFound 	= false;
 	public static int currentMaxTreeDepth 				= 0;
+	
+	
+	// Attente du main pour envoyer une reponse
+	public final static int MILLISECONDS_BEFORE_WAKE_THE_DOG 	= 4000;
+	public final static int WAITING_STEP_TIME					= 100;
+	public final static int THREAD_WAITING_STEP_TIME			= 10;
+	public static boolean keepDiggerAlive						= true;
+	public static boolean minMaxIsReadyToBeDigged				= false;
+
+	// Variables des diggers
+	// Nombre de threads qui son demarres
+	public static final int ALL_DIGGERS_ARE_DONE		= 4;
+	public static int diggersAreDone					= 0;
+	
 	}
